031628e3247d16161c8f66322676b1ffb5d50c00
Val2003 <val2003@yandex.ru>
2023-06-06 23:38:12 +0400

    add branch add-most-populars

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilmorateApplication {
-
+	//add-most-populars
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
