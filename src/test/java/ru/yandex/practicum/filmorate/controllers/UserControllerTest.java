ddcb2d314949fef4c014f15354c905b9f070048d
CyberCoHuK <108213849+CyberCoHuK@users.noreply.github.com>
2023-06-08 16:40:55 +0300

    Update UserControllerTest.java

--- src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controllers/UserControllerTest.java
@@ -21,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @SpringBootTest
 public class UserControllerTest {
     private UserController userController;
-    private DirectorStorage directorStorage;
     private User user;
     private User user2;
     private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
