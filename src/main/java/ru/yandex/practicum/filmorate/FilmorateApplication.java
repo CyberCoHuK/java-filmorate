d7ebaf021b1772ba80555b9fb992b6a87a5c5271
Val2003 <55923298+Val2003@users.noreply.github.com>
2023-06-13 15:29:46 +0400

    Update FilmorateApplication.java
    delete comment

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FilmorateApplication {
-	//add-most-populars
+public class FilmorateApplication {	
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
